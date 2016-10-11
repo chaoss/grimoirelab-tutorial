736587cfecb3ef35b184fa3c3bca7676fb2c1f74
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2016-10-11 02:03:57 +0200

    Add a Python example for the Perceval GitHub backend.

---
+++ perceval/scripts/perceval_github_1.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import argparse
+
+import perceval.backends
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description = "Simple parser for GitHub issues and pull requests"
+    )
+parser.add_argument("-t", "--token",
+                    help = "GitHub token")
+parser.add_argument("-r", "--repo",
+                    help = "GitHub repository, as 'owner/repo'")
+args = parser.parse_args()
+
+# Owner and repository names
+(owner, repo) = args.repo.split('/')
+
+# create a Git object, pointing to repo_url, using repo_dir for cloning
+repo = perceval.backends.github.GitHub(owner=owner, repository=repo,
+                                        backend_token=args.token)
+# fetch all issues/pull requests as an iteratoir, and iterate it printing
+# their number, and whether they are issues or pull requessts
+for item in repo.fetch():
+    if 'pull_request' in item['data']:
+        kind = 'Pull request'
+    else:
+        kind = 'Issue'
+    print(item['data']['number'], ':', kind)
