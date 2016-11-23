eb849d1ca84c486c2a7f841f03f7737f5739522f
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2016-11-23 11:41:47 +0100

    Print only first 60 chars.

--- perceval/scripts/perceval_mbox_1.py
+++ perceval/scripts/perceval_mbox_1.py
@@ -10,6 +10,6 @@ mbox_dir = 'archives'
 
 # create a mbox object, using mbox_uri as label, mbox_dir as directory to scan
 repo = perceval.backends.mbox.MBox(uri=mbox_uri, dirpath=mbox_dir)
-# fetch all messages as an iteratoir, and iterate it printing each subject
+# fetch all messages as an iteratoir, and print first 60 chars for each subject
 for commit in repo.fetch():
-    print(commit['data']['Subject'])
+    print(commit['data']['Subject'][0:59])
