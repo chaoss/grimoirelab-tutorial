3db8514a2fc1aae04cef3add3a60d9489789e874
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2016-12-18 19:56:21 +0100

    New example using DSL to query a git index.

---
+++ python/scripts/perceval_elasticsearch_git_dsl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+## Copyright (C) 2016 Bitergia
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## Authors:
+##   Jesus M. Gonzalez-Barahona <jgb@bitergia.com>
+##
+
+import elasticsearch
+import elasticsearch_dsl
+
+# ElasticSearch instance (url)
+es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
+
+# Query messages index, getting all items in it
+request = elasticsearch_dsl.Search(using=es, index='commits',
+                                    doc_type='summary')
+response = request.scan()
+for commit in response:
+    print(commit.hash, commit.author_date, commit.author)
