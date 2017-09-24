7fd7fb50505a42ce9e2408f5c6e225b023b8c1cc
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2017-09-24 23:31:03 +0200

    New version, now works continuously.

--- tools-and-tips/scripts/elastic_last.py
+++ tools-and-tips/scripts/elastic_last.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-## Copyright (C) 2016 Bitergia
+## Copyright (C) 2016, 2017 Bitergia
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@ import argparse
 import json
 import logging
 import operator
+import time
 import urllib3
 import warnings
 
@@ -41,10 +42,23 @@ Example:
 def parse_args ():
 
     parser = argparse.ArgumentParser(description = description)
-    parser.add_argument("field", type=str,
-                        help = "Date field for selecting latest items")
     parser.add_argument("urls", type=str, nargs='+',
                         help = "urls of the indexes to consider")
+    parser.add_argument("--date_field", type=str,
+                        default='metadata__timestamp',
+                        help = "Date field for selecting latest items")
+    parser.add_argument("--count", type=int, default=50,
+                        help = "Number of items to get from each index")
+    parser.add_argument("--total", type=int, default=40,
+                        help = "Number of items to produce in total")
+
+
+    parser.add_argument("--loop", type=int,
+                        help = "Loop, refreshing data every loop seconds")
+    parser.add_argument("--output", type=str,
+                        default='events.json',
+                        help = "Output file name")
+
     parser.add_argument("--verify_certs", dest="verify_certs",
                         action="store_true",
                         help = "Verify ssl certificates (default)")
@@ -83,40 +97,62 @@ def log_format (args):
     else:
         logging.basicConfig(format=format, level=level)
 
+def last_items(url, verify_certs, date_field, count):
+    """Get last items from index specified by url.
+
+    """
+
+    (es_url, index) = url.rsplit('/', 1)
+    try:
+        es = elasticsearch.Elasticsearch([es_url],
+                                        verify_certs=verify_certs)
+    except elasticsearch.exceptions.ImproperlyConfigured as exception:
+        if exception.args[0].startswith("Root certificates are missing"):
+            logging.error("Error validating SSL certificate for {}." \
+                        .format(self.instance))
+            logging.error("Use --no_verify_certs to avoid validation.")
+            exit()
+        else:
+            raise
+    request = elasticsearch_dsl.Search(using=es, index=index)
+    request = request.sort('-' + date_field)
+    request = request[0:count]
+    try:
+        response = request.execute()
+    except elasticsearch.exceptions.RequestError as e:
+        logging.error("Error fetching " + url)
+        logging.error(e)
+        exit()
+    except:
+        logging.error("Other exception caught")
+        raise
+    results = []
+    for item in response:
+        item['date'] = item[date_field]
+        results.append(item.to_dict())
+    return results
+
 def main():
     args = parse_args()
     log_format(args)
 
-    results = []
-    for url in args.urls:
-        (es_url, index) = url.rsplit('/', 1)
-        try:
-            es = elasticsearch.Elasticsearch([es_url],
-                                            verify_certs=args.verify_certs)
-        except elasticsearch.exceptions.ImproperlyConfigured as exception:
-            if exception.args[0].startswith("Root certificates are missing"):
-                logging.error("Error validating SSL certificate for {}." \
-                            .format(self.instance))
-                logging.error("Use --no_verify_certs to avoid validation.")
-                continue
-            else:
-                raise
-        request = elasticsearch_dsl.Search(using=es, index=index)
-        request = request.sort('-' + args.field)
-        request = request[0:20]
-        try:
-            response = request.execute()
-        except elasticsearch.exceptions.RequestError as e:
-            logging.error("Error fetching " + url)
-            logging.error(e)
-            continue
-        except:
-            logging.error("Other exception caught")
-        for item in response:
-            results.append(item.to_dict())
-    sorted_results = sorted(results, key=operator.itemgetter(args.field))
-    json_results = json.dumps(sorted_results, sort_keys=True, indent=2)
-    print(json_results)
+    loop = True
+    while loop:
+        results = []
+        for url in args.urls:
+            results.extend(last_items(url, args.verify_certs,
+                        args.date_field, args.count))
+        sorted_results = sorted(results, key=operator.itemgetter(args.date_field))
+        output_results = sorted_results[-args.total:]
+        json_results = json.dumps(output_results, sort_keys=True, indent=2)
+        with open(args.output, 'w') as f:
+            f.write(json_results)
+            print("Items read: ", len(sorted_results),
+                "written: ", len(output_results))
+        if args.loop:
+            time.sleep(args.loop)
+        else:
+            loop = False
 
 if __name__ == "__main__":
     main()
--- tools-and-tips/scripts/events.js
+++ tools-and-tips/scripts/events.js
@@ -1,66 +1,146 @@
-// Get events.json, and load it in the HTML page
+// Get events.json, and load it in the HTML page as a vertical marqee
+// Do that continuously, checking for new events in new events.json
 //
 
-function decorate_git (event) {
-  var author = event['author_name'];
-  var org = event['author_org_name'];
-  var project = event['project'];
-  var repo = event['repo_name'];
-  var hash = event['hash'].slice(0,7);
-  var commit_date = new Date(event['utc_commit'])
-  var author_date = new Date(event['utc_author'])
-  var message = event['message']
-    .replace(/</g,'&lt;').replace(/>/g,'&gt;')
-    .replace(/(?:\r\n|\r|\n)/g,'<br>');
+// Copyright (C) 2017 Bitergia
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// Authors:
+//   Jesus M. Gonzalez-Barahona <jgb@bitergia.com>
+//
 
-  var text = '<p class="head">Commit: <b>' + hash + '</b> ';
-  text += ' (' + project + ', <em>' + repo + '</em>)</p>\n';
-  text += '<p class="author">Author: ' + author + ' (' + org + ')</p>\n';
-  text += '<p class="date">Commit date: <em>' + String(commit_date)
-    + '</em></p>\n';
-  text += '<p class="date">Author date: <em>' + String(author_date)
-    + '</em></p>\n';
-  time_diff = (commit_date.getTime() - author_date.getTime()) / (1000 * 3600 * 24)
-  text += '<p class="date">Days from authorship to commit: <em>'
-    + time_diff.toFixed(2) + '</em></p>\n';
-  text += '<p class="message">' + message + '<p>\n';
+(function() {
 
-  return(text)
-}
+  // Variable for events to show in marquee
+  var events = [];
+  // Element for the marquee
+  var marquee_el;
+  // Last height of marquee_el
+  var marquee_height = 0;
 
-function decorate_default (event) {
-  var text = `<p class="default">`;
-  for (var component in event) {
-    text += component + ': ';
-    text += String(data[event][component])
-      .replace(/</g,'&lt;').replace(/>/g,'&gt;');
-    text += '</p>\n';
+  // Decorate a git event (produce HTML from it)
+  function decorate_git (event) {
+    var date = new Date(event['date']);
+    var author = event['author_name'];
+    var org = event['author_org_name'];
+    var project = event['project'];
+    var repo = event['repo_name'];
+    var hash = event['hash'].slice(0,7);
+    var commit_date = new Date(event['utc_commit'])
+    var author_date = new Date(event['utc_author'])
+    var message = event['message']
+      .replace(/</g,'&lt;').replace(/>/g,'&gt;')
+      .replace(/(?:\r\n|\r|\n)/g,'<br>');
+
+    var text = '<p class="head">Commit: <b>' + hash + '</b> ';
+    text += ' (' + project + ', <em>' + repo + '</em>)</br>\n';
+    text += String(date) + '</p>\n';
+    text += '<p class="author">Author: ' + author + ' (' + org + ')</p>\n';
+    text += '<p class="date">Commit date: <em>' + String(commit_date)
+      + '</em></p>\n';
+    text += '<p class="date">Author date: <em>' + String(author_date)
+      + '</em></p>\n';
+    time_diff = (commit_date.getTime() - author_date.getTime()) / (1000 * 3600 * 24)
+    text += '<p class="date">Days from authorship to commit: <em>'
+      + time_diff.toFixed(2) + '</em></p>\n';
+    text += '<p class="message">' + message + '<p>\n';
+
+    return(text)
   }
-  return(text)
-}
 
-function update () {
-  $.getJSON("events.json", function(data) {
-    console.log("Update");
-    var events = '<div class="marquee">';
-    for (var event = 0; event < data.length; event++) {
-      var event_str = '<div class="event">';
-      if ('is_git_commit' in data[event]) {
-        event_str += decorate_git(data[event]);
-      } else {
-        event_str += decorate_default(data[event]);
-      }
-      event_str += '</div>\n'
-      events += event_str + '\n';
+  // Decorate a "generic" event (produce HTML from it)
+  function decorate_default (event) {
+    var text = `<p class="default">`;
+    for (var component in event) {
+      text += component + ': ';
+      text += String(data[event][component])
+        .replace(/</g,'&lt;').replace(/>/g,'&gt;');
+      text += '</p>\n';
     }
-    events += '</div>\n';
-    $('#events').html(events);
-    $('.marquee').css('animation-duration', 4*event + 's')
-	});
-}
+    return(text);
+  }
 
-$(document).ready(function(){
-  $.ajaxSetup({cache: false});
-  update();
-  setInterval(update, 100000);
-});
+  // Update the text to appear in the marquee with some events
+  function update_marquee (el, events) {
+    var event_str;
+    var event_el;
+
+    for (var event = 0; event < events.length; event++) {
+      if ('is_git_commit' in events[event]) {
+        event_str = decorate_git(events[event]);
+      } else {
+        event_str = decorate_default(events[event]);
+      }
+      event_el = $('<div />').addClass('event').html(event_str);
+      el.append(event_el);
+    };
+  };
+
+  // Update the marquee.
+  // get events.json, decide which ones are new events,
+  // decorate those, update the marquee, and call itself,
+  // for further updating
+  function update () {
+    $.getJSON("events.json", function(data) {
+      var new_events = [];
+
+      console.log("Update");
+      if (events.length == 0) {
+        console.log("Intializing events")
+        events = data;
+        new_events = data;
+      } else {
+        console.log("Considering updating events")
+        last_date = events[events.length-1]['date'];
+        for (var event = 0; event < data.length; event++) {
+          if (data[event]['date'] > last_date) {
+            console.log("Updating event")
+            events.push(data[event])
+            new_events.push(data[event]);
+          };
+        };
+      };
+      update_marquee(marquee_el, new_events);
+      var new_marquee_height = marquee_el.height();
+      var inc_marquee_height = new_marquee_height - marquee_height;
+      marquee_height = new_marquee_height;
+      console.log(marquee_height, inc_marquee_height);
+      if (inc_marquee_height > 0) {
+        var timeout = inc_marquee_height * 50;
+        marquee_el.animate({
+          bottom: marquee_el.height()-150
+        }, {
+          duration: timeout,
+          easing: "linear",
+          complete: update
+        });
+      } else {
+        setTimeout(update, 1000*10);
+      };
+  	});
+  }
+
+  // When the DOM is ready, start the show
+  $(document).ready(function(){
+    $.ajaxSetup({cache: false});
+    var events_el = $('#events').first();
+    events_el.html('<div class="marquee"></div>');
+    marquee_el = events_el.children().first();
+    console.log(events_el.height());
+    update();
+  });
+
+})();
