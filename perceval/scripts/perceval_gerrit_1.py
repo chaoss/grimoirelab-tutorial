e14093a4bb924ee66ac15d527582ac7a977d2516
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2018-04-03 23:20:33 +0200

    Simple script to retrieve data from Gerrit.

---
+++ perceval/scripts/perceval_gerrit_1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from datetime import datetime, timedelta
+
+from perceval.backends.core.gerrit import Gerrit
+
+# hostname of the Gerrit instance
+hostname = 'gerrit.opnfv.org'
+# user for sshing to the Gerrit instance
+user = 'jgbarah'
+# retrieve only reviews changed since this date
+from_date = datetime.now() - timedelta(days=1)
+# create a Gerrit object, pointing to hostname, using user for ssh access
+repo = Gerrit(url=hostname, user=user)
+
+# fetch all reviews as an iterator, and iterate it printing each review id
+for review in repo.fetch(from_date=from_date):
+    print(review['data']['number'])
